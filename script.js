053f89d58b9677b20846f36fd88b0b3852fb065f
Paulo Marques <pauloalmarques@hotmail.com>
2025-04-01 23:22:33 -0300

    Update Incerto do Carrossel

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const swiper = new Swiper('.swiper', {
     loop: true,
+    spaceBetween: 30,
   
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
     },
   
     // Navigation arrows
@@ -15,6 +18,19 @@ const swiper = new Swiper('.swiper', {
     // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+        360: {
+            slidesPerView: 1,
+        },
+        594: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
     },
   });
   
